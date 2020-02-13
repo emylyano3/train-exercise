1214badb21cfe38271e834dba30bd28f3f121122
emylyano3 <emiliano.schiano@gmail.com>
2020-02-13 14:54:03 -0300

    new tests

--- src/test/java/mule/GraphLoaderTest.java
+++ src/test/java/mule/GraphLoaderTest.java
@@ -2,6 +2,8 @@ package mule;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 import org.junit.Test;
 
@@ -13,8 +15,40 @@ import mule.graph.model.IGraph;
 public class GraphLoaderTest {
 
 	@Test
-	public void load () throws FileNotFoundException {
+	public void load_FILE () throws FileNotFoundException {
 		IGraph g = new GraphLoader().loadGraph(new File(getClass().getResource("/mulevania.graph").getFile()));
+		assertNotNull(g);
+		assertNotNull(g.getEdges());
+		assertFalse(g.getEdges().isEmpty());
+		assertEquals(9, g.getEdges().size());
+		assertFalse(g.getNodes().isEmpty());
+		assertEquals(5, g.getNodes().size());
+		assertNotNull(g.getNode("A"));
+		assertEquals("A", g.getNode("A").getName());
+	}
+
+	@Test
+	public void load_STRING () throws IOException {
+		InputStream is = getClass().getResourceAsStream("/mulevania.graph");
+		byte[] bytes = new byte[is.available()];
+		is.read(bytes);
+		IGraph g = new GraphLoader().loadGraph(new String(bytes));
+		assertNotNull(g);
+		assertNotNull(g.getEdges());
+		assertFalse(g.getEdges().isEmpty());
+		assertEquals(9, g.getEdges().size());
+		assertFalse(g.getNodes().isEmpty());
+		assertEquals(5, g.getNodes().size());
+		assertNotNull(g.getNode("A"));
+		assertEquals("A", g.getNode("A").getName());
+	}
+
+	@Test
+	public void load_BYTES () throws IOException {
+		InputStream is = getClass().getResourceAsStream("/mulevania.graph");
+		byte[] bytes = new byte[is.available()];
+		is.read(bytes);
+		IGraph g = new GraphLoader().loadGraph(bytes);
 		assertNotNull(g);
 		assertNotNull(g.getEdges());
 		assertFalse(g.getEdges().isEmpty());
